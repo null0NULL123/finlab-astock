ed96e3a825ad942f620f1ecd410bacae017427c6
null0NULL123 <1872456114@qq.com>
2024-05-19 12:00:00 +0800

    feat: initialize ETF data module

---
+++ src/data/etf/__init__.py
@@ -0,0 +1,18 @@
+"""
+ETF 数据模块
+
+提供 ETF 列表获取、历史净值、持仓、规模等数据获取功能，
+以及技术指标计算、持仓相似度分析、聚类等分析功能。
+"""
+
+from .fetcher import EtfListFetcher
+from .holdings_fetcher import EtfHoldingsFetcher
+from .nav_fetcher import EtfNavFetcher
+from .scale_fetcher import EtfScaleFetcher
+
+__all__ = [
+    "EtfListFetcher",
+    "EtfHoldingsFetcher",
+    "EtfNavFetcher",
+    "EtfScaleFetcher",
+]

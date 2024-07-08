d6bc81aa1419c931ad170740cade8c6bf91d3038
null0NULL123 <1872456114@qq.com>
2024-07-08 12:00:00 +0800

    feat: initialize SW level2 module

---
+++ src/data/sw_level2/__init__.py
@@ -0,0 +1,27 @@
+"""
+申万二级行业数据模块
+
+提供申万二级行业数据获取、分析和可视化的工具集。
+"""
+
+from .utils import (
+    apply_date_parent_filters,
+    compute_corr_matrix,
+    compute_returns,
+    extract_top_pairs,
+    load_square_matrix,
+    pivot_returns,
+    read_daily_metrics,
+    write_cluster_summary,
+)
+
+__all__ = [
+    "apply_date_parent_filters",
+    "compute_corr_matrix",
+    "compute_returns",
+    "extract_top_pairs",
+    "load_square_matrix",
+    "pivot_returns",
+    "read_daily_metrics",
+    "write_cluster_summary",
+]
